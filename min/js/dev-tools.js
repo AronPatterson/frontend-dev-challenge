2637505e869c10bdc0a5272762347d7a31d2be1f
AronPatterson <apatterson@aroonline.com>
2016-08-21 11:49:44 -0500

    Setting up the site structure finished - opted out of using Flex Grid due to issues with the mega nav dropdown and flex. Next step: get the JSON to pull in properly.

--- min/js/dev-tools.js
+++ min/js/dev-tools.js
@@ -15,15 +15,44 @@ $( '#mainNav li' ).each(function() { // add an active class if on the current pa
 
 // Back to Top Button
 $(document).ready(function(a) {
-	var t = 300,
-		n = 1200,
-		d = 700,
-		i = a( '.backToTopBtn' );
-	a(window).scroll(function() {
+	var t = 300, // px height on page where it starts to show up
+		n = 1200, // px height where it'll fade out
+		d = 700, // 
+		i = a( '.backToTopBtn' ); // attaches it to the correct class
+	a(window).scroll(function() { // if you scroll, it checks to see your document height
 		a(this).scrollTop() > t ? i.addClass( 'bttVisible' ) : i.removeClass( 'bttVisible bttFadeOut' ), a(this).scrollTop() > n && i.addClass( 'bttFadeOut' )
-	}), i.on( 'click', function( t ) {
+	}), i.on( 'click', function( t ) { // when you click it, it animates the scroll up instead of the jarring instantaneous acnhor effect
 		t.preventDefault(), a( 'body,html' ).animate({
 			scrollTop: 0
 		}, d)
 	})
+});
+
+// Load JSON Content
+$.getJSON( "data.json", function( data ) {
+	var items = [];
+	$.each( data, function( key, val ) {
+		items.push( "<li id='" + key + "'>" + val + "</li>" );
+	});
+
+	$( "<ul/>", {
+		"class": "my-new-list",
+		html: items.join( "" )
+	}).appendTo( "body" );
+});
+
+//start ajax request
+$.ajax({
+    url: "data.json",
+    //force to handle it as text
+    dataType: "text",
+    success: function(data) {
+        
+        //data downloaded so we call parseJSON function 
+        //and pass downloaded data
+        var json = $.parseJSON(data);
+        //now json variable contains data in json format
+        //let's display a few items
+        $('#results').html('Plugin name: ' + json.name + '<br />Author: ' + json.author.name);
+    }
 });
